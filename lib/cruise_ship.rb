c52ebbbf6b202347efee315c1db07e5dd06d3f43
chic-caller-7520 <arbrown110@gmail.com>
2020-04-04 04:03:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,6 +9,7 @@
 
 def select_winner(passengers)
   winner = ""
+<<<<<<< HEAD
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
@@ -18,4 +19,11 @@ def select_winner(passengers)
   winner
   
 end  
+=======
+  passanger.each do |suite, name |
+  if suite == :suit_a && name.start_with?("A")  
+    winner = name 
+  end
+end
+>>>>>>> d46c8c5b78b391bce1002c878bc3655a4082c815
 
